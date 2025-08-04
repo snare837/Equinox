c7511a824f25e38b9c2494534d9d3353b4b821af
Your Full Name <snare@govst.edu>
2025-08-04 02:00:47 -0500

    Fix email/phone iss ue in EditUser

--- Migrations/20250804073307_InitialCreate.cs
+++ Migrations/20250804073307_InitialCreate.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Equinox.Migrations
 {
     /// <inheritdoc />
-    public partial class Initial : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
